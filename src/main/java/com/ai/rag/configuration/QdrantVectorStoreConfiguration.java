6b3ff8722d13ec7cac66d7bee890d3daa3424cd0
vineetaparodkar <vineetaparodkar@gmail.com>
2024-05-03 03:23:48 +0530

    adds minor fixes

--- src/main/java/com/ai/rag/configuration/QdrantVectorStoreConfiguration.java
+++ src/main/java/com/ai/rag/configuration/QdrantVectorStoreConfiguration.java
@@ -8,13 +8,11 @@ import org.springframework.ai.vectorstore.qdrant.QdrantVectorStore;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
 
 @AutoConfiguration
 @ConditionalOnClass({ QdrantVectorStore.class, EmbeddingClient.class })
-@EnableConfigurationProperties(QdrantVectorStoreProperties.class)
 public class QdrantVectorStoreConfiguration {
 
     private final ApplicationProperties applicationProperties;
